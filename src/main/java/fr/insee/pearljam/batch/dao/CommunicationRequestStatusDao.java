7302994b33f45ef298acf68f1f3410d37783f343
Simon Demaziere <simon.demaziere@insee.fr>
2025-04-10 13:50:59 +0200

    fix: remove deleted column reference

--- src/main/java/fr/insee/pearljam/batch/dao/CommunicationRequestStatusDao.java
+++ src/main/java/fr/insee/pearljam/batch/dao/CommunicationRequestStatusDao.java
@@ -2,6 +2,6 @@ package fr.insee.pearljam.batch.dao;
 
 public interface CommunicationRequestStatusDao {
 
-    void addStatus(String communicationRequestId, String status, long timestamp, String meshuggahId);
+    void addStatus(String communicationRequestId, String status, long timestamp);
 
 }
--- src/main/java/fr/insee/pearljam/batch/dao/CommunicationRequestStatusDaoImpl.java
+++ src/main/java/fr/insee/pearljam/batch/dao/CommunicationRequestStatusDaoImpl.java
@@ -13,12 +13,12 @@ public class CommunicationRequestStatusDaoImpl implements CommunicationRequestSt
     JdbcTemplate pilotageJdbcTemplate;
 
     @Override
-    public void addStatus(String communicationRequestId, String status, long timestamp, String meshuggahId) {
+    public void addStatus(String communicationRequestId, String status, long timestamp) {
         String qString = """
                 INSERT INTO communication_request_status
-                (communication_request_id, status, "date", meshuggah_id)
-                VALUES( ?, ?, ?, ?)
+                (communication_request_id, status, "date")
+                VALUES( ?, ?, ?)
                 """;
-        pilotageJdbcTemplate.update(qString, Long.parseLong(communicationRequestId), status, timestamp, meshuggahId);
+        pilotageJdbcTemplate.update(qString, Long.parseLong(communicationRequestId), status, timestamp);
     }
 }
