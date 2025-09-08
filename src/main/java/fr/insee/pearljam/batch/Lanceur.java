65aad736c5fdcb10f02d3fa106f8590b974c23d5
davdarras <david.darras@insee.fr>
2025-09-08 15:19:43 +0200

    fix: change main class name

--- src/main/java/fr/insee/pearljam/batch/Lanceur.java
+++ src/main/java/fr/insee/pearljam/batch/Lanceur.java
@@ -5,9 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "fr.insee.pearljam.batch")
-public class PearlJamBatchApplication {
+public class Lanceur {
     public static void main(String[] args) {
-        var ctx = SpringApplication.run(PearlJamBatchApplication.class, args);
+        var ctx = SpringApplication.run(Lanceur.class, args);
         int code = SpringApplication.exit(ctx);
         System.exit(code);
     }
